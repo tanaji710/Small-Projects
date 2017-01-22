dd8225f2031fadfd201977ff113da5386c56dd13
tanaji710 <yedagetanaji@gmail.com>
2017-01-22 17:21:11 +0530

    Add files via upload

---
+++ Web_Calculator/Web_Calculator/Calculator.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="calculator_web.Main" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <style type="text/css">
+        .btn
+        {
+            height: 50px;
+            width: 50px;
+            background: white;
+        }
+        
+        .btn_tr
+        {
+            text-align: center;
+        }
+        .style1
+        {
+            height: 112px;
+        }
+    </style>
+</head>
+<body bgcolor="Black">
+    <form id="form1" runat="server"><center>
+    <br />
+    <br /><h1 style="color: #FF0000">Basic Calculator</h1>
+    
+    
+    
+    <table border="1" class=" table" style="height: 400px; width: 300px;">
+        
+        
+        <tr>
+            <td colspan="4" style="text-align: right; color: #FFFFFF;" class="style1" >
+                <asp:Label ID="lblDisplay" runat="server" BorderColor="Black" Font-Size="26" Text="0"></asp:Label>
+            </td><hr />
+        </tr>
+        <tr class="btn_tr">
+            
+            <td>
+                <asp:Button ID="btnCancel" runat="server" Text="C" CssClass="btn" 
+                    onclick="btnCancel_Click" />
+            </td>
+        </tr>
+        <tr class="btn_tr">
+            <td>
+                <asp:Button ID="btnNum7" runat="server" Text="7" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnNum8" runat="server" Text="8" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnNum9" runat="server" Text="9" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnDivide" runat="server" Text="/" CssClass="btn" />
+            </td>
+        </tr>
+        <tr class="btn_tr">
+            <td>
+                <asp:Button ID="btnNum4" runat="server" Text="4" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnNum5" runat="server" Text="5" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnNum6" runat="server" Text="6" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnMutiply" runat="server" Text="*" CssClass="btn" />
+            </td>
+        </tr>
+        <tr class="btn_tr">
+            <td>
+                <asp:Button ID="btnNum1" runat="server" Text="1" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnNum2" runat="server" Text="2" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnNum3" runat="server" Text="3" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnMinus" runat="server" Text="-" CssClass="btn" />
+            </td>
+        </tr>
+        <tr class="btn_tr">
+            <td>
+                <asp:Button ID="btnNum0" runat="server" Text="0" CssClass="btn" />
+            </td>
+            <td>
+                <asp:Button ID="btnPoint" runat="server" Text="." CssClass="btn" 
+                    onclick="btnPoint_Click" />
+            </td>
+            <td>
+                <asp:Button ID="btnEqual" runat="server" Text="=" CssClass="btn" 
+                    onclick="btnEqual_Click" />
+            </td>
+            <td>
+                <asp:Button ID="btnPlus" runat="server" Text="+" CssClass="btn" />
+            </td>
+        </tr>
+        
+    </table></center>
+    </form>
+    <h3 style="color: #FFFFFF; text-align: center">Developed by Tanajee Yedage</h3>
+</body>
+</html>
---
+++ Web_Calculator/Web_Calculator/Calculator.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+namespace calculator_web
+{
+    public partial class Main : System.Web.UI.Page
+    {
+
+        private enum CalculatorStateType
+        {
+            Zero, NumberWithoutPoint, NumberWithPoint, Operator, Finish
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CurrentState = CalculatorStateType.Zero;
+            }
+
+            for (int i = 0; i <= 9; i++)
+            {
+                (FindControl("btnNum" + i) as Button).Click += btnNumStar_Click;
+            }
+
+            btnPlus.Click += btnOperator_Click;
+            btnMinus.Click += btnOperator_Click;
+            btnMutiply.Click += btnOperator_Click;
+            btnDivide.Click += btnOperator_Click;
+        }
+
+        
+
+        
+        private void btnOperator_Click(object sender, EventArgs e)
+        {
+            //Debug.WriteLine("operator");
+            Button btnSender = sender as Button;
+
+            if (CurrentState == CalculatorStateType.Zero ||
+                CurrentState == CalculatorStateType.NumberWithPoint ||
+                CurrentState == CalculatorStateType.NumberWithoutPoint)
+            {
+                FirstOperand = double.Parse(lblDisplay.Text);
+                Operator = char.Parse(btnSender.Text);
+
+                CurrentState = CalculatorStateType.Operator;
+            }
+            else if (
+                CurrentState == CalculatorStateType.Finish || 
+                CurrentState == CalculatorStateType.Zero)
+            {
+                return;
+            }
+        }
+
+
+        private void btnNumStar_Click(object sender, EventArgs e)
+        {
+            Button btnSender = sender as Button;
+
+            if (CurrentState == CalculatorStateType.Zero || 
+                CurrentState == CalculatorStateType.Operator ||
+                CurrentState == CalculatorStateType.Finish)
+            {
+                lblDisplay.Text = btnSender.Text;
+
+                if (btnSender.Text != "0")
+                {
+                    CurrentState = CalculatorStateType.NumberWithoutPoint;
+                }
+            }
+            else if (
+                CurrentState == CalculatorStateType.NumberWithoutPoint ||
+                CurrentState == CalculatorStateType.NumberWithPoint)
+            {
+                lblDisplay.Text += btnSender.Text;
+            }
+        }
+
+        protected void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (CurrentState == CalculatorStateType.Finish ||
+                CurrentState == CalculatorStateType.Operator)
+            {
+                return;
+            }
+            else if (
+                CurrentState == CalculatorStateType.Zero ||
+                CurrentState == CalculatorStateType.NumberWithPoint ||
+                CurrentState == CalculatorStateType.NumberWithoutPoint)
+            {
+                if (FirstOperand != null && Operator != null)
+                {
+                    SecondOperand = double.Parse(lblDisplay.Text);
+                    lblDisplay.Text = string.Format("{0:G}", Calculate(FirstOperand.Value, SecondOperand.Value, Operator.Value));
+
+                    CurrentState = CalculatorStateType.Finish;
+                    FirstOperand = null;
+                    Operator = null;
+                    SecondOperand = null;
+                }
+            }
+        }
+
+        protected void btnPoint_Click(object sender, EventArgs e)
+        {
+            if (CurrentState == CalculatorStateType.Finish ||
+                CurrentState == CalculatorStateType.NumberWithPoint ||
+                CurrentState == CalculatorStateType.Operator)
+            {
+                return;
+            }
+            else if (
+                CurrentState == CalculatorStateType.Zero ||
+                CurrentState == CalculatorStateType.NumberWithoutPoint
+                )
+            {
+                lblDisplay.Text += ".";
+
+                CurrentState = CalculatorStateType.NumberWithPoint;
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            CurrentState = CalculatorStateType.Zero;
+            FirstOperand = null;
+            SecondOperand = null;
+            Operator = null;
+            lblDisplay.Text = "0";
+        }
+
+        #endregion
+
+        private double Calculate(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b != 0) return a / b;
+                    else return 0;
+            }
+            return 0;
+        }
+    }
+}
